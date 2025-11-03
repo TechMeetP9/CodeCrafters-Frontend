76960564d6f3ebb6af6dd21cda78a1db12da8599
Gabi <gagallegosanda@gmail.com>
2025-11-03 13:48:39 +0100

    Background component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './styles/main.scss'
+import { useState } from 'react';
+import "./styles/main.scss";
+import Background from './components/Background/background';
+
 
 function App() {
   
 
   return (
     <>
-      
+      <Background />
     </>
   )
 }
