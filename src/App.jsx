537aecd661703a9964986777daaf68fb8b2ac41c
Gabi <gagallegosanda@gmail.com>
2025-11-04 14:53:50 +0100

    search bar component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import "./styles/main.scss";
 import Background from "./components/Background/background";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
+import SearchBar from "./components/searchbar/searchbar";
 
 function App() {
   return (
     <>
       <Background />
       <Navbar />
-      <Footer/>
+      
+      {/* <Footer /> */}
+    
+
     </>
   );
 }
