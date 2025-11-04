0186ed2e167d5292ce0f934bd99c27dde2b839f3
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-11-04 09:25:14 +0100

    feat/(footer)footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./styles/main.scss";
 import Background from "./components/Background/background";
 import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <>
       <Background />
       <Navbar />
+      <Footer/>
     </>
   );
 }
