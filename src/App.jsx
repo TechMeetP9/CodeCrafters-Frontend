e252419e010398abdb672fb89b7c5e0dcd3b4a06
Gabi <gagallegosanda@gmail.com>
2025-11-04 14:57:26 +0100

    erase import searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./styles/main.scss";
 import Background from "./components/Background/background";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
-import SearchBar from "./components/searchbar/searchbar";
+
 
 function App() {
   return (
