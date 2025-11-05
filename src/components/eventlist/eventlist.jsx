cbb986eb3dadcec8b5754db2112f58ad148f5df8
Gabi <gagallegosanda@gmail.com>
2025-11-05 18:42:35 +0100

    landing page first half created

--- src/components/eventlist/eventlist.jsx
+++ src/components/eventlist/eventlist.jsx
@@ -6,8 +6,7 @@ const EventList = () => {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    // 🔹 Aquí más adelante pondrás el fetch al backend. 
-    // Por ahora usamos datos de ejemplo:
+    // 🔹 Datos de ejemplo temporales aqui se conecta con el API BACKEND
     const mockEvents = [
       {
         id: 1,
@@ -26,6 +25,42 @@ const EventList = () => {
         eventTime: "19h00 CET",
         eventCreator: "Berlin Design Hub",
         eventAttendees: 80,
+      },
+      {
+        id: 3,
+        eventImage: "https://picsum.photos/300/150?random=3",
+        eventName: "Workshop UX/UI Avanzado",
+        eventDate: "Enero 14-15",
+        eventTime: "10h00 CET",
+        eventCreator: "UX Studio BCN",
+        eventAttendees: 45,
+      },
+      {
+        id: 4,
+        eventImage: "https://picsum.photos/300/150?random=4",
+        eventName: "Festival de Diseño Sostenible",
+        eventDate: "Febrero 22-24",
+        eventTime: "09h30 CET",
+        eventCreator: "Green Design Europe",
+        eventAttendees: 200,
+      },
+      {
+        id: 5,
+        eventImage: "https://picsum.photos/300/150?random=5",
+        eventName: "Expo Arte Digital",
+        eventDate: "Marzo 10-12",
+        eventTime: "17h00 CET",
+        eventCreator: "Digital Art Berlin",
+        eventAttendees: 95,
+      },
+      {
+        id: 6,
+        eventImage: "https://picsum.photos/300/150?random=6",
+        eventName: "Charla: Creatividad e Innovación",
+        eventDate: "Abril 2",
+        eventTime: "19h30 CET",
+        eventCreator: "Gabi Design Talks",
+        eventAttendees: 60,
       },
     ];
 
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,20 +1,63 @@
-import React from 'react'
-import Background from '../../components/Background/background'
-import Navbar from '../../components/navbar/navbar'
-import Footer from '../../components/footer/footer'
+// Landing.jsx
+import React from "react"; 
+import "./landing.scss"; 
+import Background from "../../components/background/background"; 
+import Navbar from "../../components/navbar/navbar"; 
+import SearchBar from "../../components/searchbar/searchbar"; 
+import SignupButton from "../../components/signupbutton/signupbutton"; 
+import EventList from "../../components/eventlist/eventlist"; 
+import Footer from "../../components/footer/footer"; 
+import Whitebutton from "../../components/whitebutton/whitebutton";
 
-function Landing() {
-  return (
-    <section className="landing">
-      <Background />
-      <Navbar />
-      <main>
-      <h1>Join Tech Events Today</h1>
+const Landing = () => { 
+  return ( 
+    <div className="landing"> 
+      <Background /> 
+      <Navbar /> 
+
+      <main className="landing__content"> 
+        <section className="hero"> 
+          <div className="hero__text"> 
+            <h1 className="hero__title"> 
+              Join Tech Events Today 
+            </h1>
+            <div className="hero__search"> 
+              <SearchBar /> 
+            </div>
+            <div className="hero__cta"> 
+              <SignupButton variant="secondary"> 
+                Join us 
+              </SignupButton>
+            </div>
+          </div>
+
+          <div className="hero__image"> 
+            <img
+              src="/src/assets/heroimage.png" 
+              alt="Community of people attending tech events" 
+              className="hero__image-el" 
+            />
+          </div>
+        </section>
+        <section className="events"> 
+        <div className="events__header"> 
+          <button className="events__see-all"> 
+            See all events 
+          </button>
+        </div>
+
+        <div className="events__grid"> 
+          <EventList /> 
+        </div>
+        </section>
+        {/* // aquí abajo me dijiste "van otros componentes que aún no diseño (no pongas nada aún)" */}
+        {/* // así que no pongo nada más en el main */}
+
       </main>
-{/* <Footer /> */}
 
-    </section>
-  )
-}
+      <Footer /> 
+    </div>
+  );
+};
 
-export default Landing
+export default Landing; // // exportamos el componente para poder usarlo en las rutas o en App
